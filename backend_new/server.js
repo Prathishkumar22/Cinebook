68f74fdc427cf88c99b682d4f234428c019d5c35
Prathishkumar22 <167306834+Prathishkumar22@users.noreply.github.com>
2025-11-03 12:25:46 +0530

    commited

--- backend_new/server.js
+++ backend_new/server.js
@@ -6,12 +6,18 @@ import connectDB from './config/db.js';
 import dotenv from 'dotenv';
 import User from './models/user.model.js';
 import routes from './routes/auth.route.js';
+import path from 'path';
 dotenv.config();
 import cors from 'cors';
 app.use(cors({
   origin: "http://localhost:5173",  // your React app URL
   credentials: true
 }));
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, '/Cinebook/dist')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, "Cinebook", "dist", "index.html"));
+});
 
 app.use(express.json());
 app.use('/api/users', routes);
